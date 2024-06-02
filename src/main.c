c04456e7660df2c567aba96ece4693d508f5aac4
jirka3jaluvka <jirka3jaluvka@email.cz>
2024-06-02 19:32:28 +0200

    minor

--- src/main.c
+++ src/main.c
@@ -6,38 +6,53 @@
 //#include "delay.h"
 //#include "uart1.h"
 
+    int32_t latitude = 495539350;
+    int32_t longitude = 171031850;
+    int32_t lockedLatitude = 495539350;
+    int32_t lockedLongitude = 171031850;
+
 void init(void)
 {
-    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
-
-    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-#if defined (BTN_PORT) || defined (BTN_PIN)
-    GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
-#endif
-
     init_milis();
-    //init_uart1();
+    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
+    GPIO_Init(GPIOD, GPIO_PIN_4, GPIO_MODE_OUT_PP_LOW_SLOW);
+
+    TIM2_TimeBaseInit(TIM2_PRESCALER_256, 1250);
+    TIM2_OC1Init(TIM2_OCMODE_PWM1,TIM2_OUTPUTSTATE_ENABLE, 1 , TIM2_OCPOLARITY_LOW);
+    TIM2_OC1PreloadConfig(ENABLE);
+    TIM2_Cmd(ENABLE);
 }
 
+int getDiff(now,locked){
+    int16_t x = now - locked;
+    return x; 
+}
+
+int getAngle(){
+    int16_t a = getDiff(latitude,lockedLatitude);
+    int16_t b = getDiff(longitude,lockedLongitude);
+    int16_t x =
+}
 
 int main(void)
 {
-  
+
     uint32_t time = 0;
+    uint16_t value = 100;
 
     init();
 
     while (1) {
-#if defined (BTN_PORT) && defined (BTN_PIN)
-        if (milis() - time > 333 && !PUSH(BTN)) {
-#else
-        if (milis() - time > 333 ) {
-#endif
-            REVERSE(LED); 
-            time = milis();
-            //printf("%ld\n", time);
-        }
-        //delay_ms(333);
+
+
+
+            if (milis() - time > 100 ) {
+                TIM2_SetCompare1(value++);
+
+                time = milis();
+
+                }
+        if (value>1000) {value=10;}
     }
 }
 
