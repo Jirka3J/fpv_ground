8507dc6b1924b90d3365aa5d9b25b6f95d662820
jirka3jaluvka <jirka3jaluvka@email.cz>
2024-06-16 18:43:28 +0200

    degrees to pwm implemented

--- src/main.c
+++ src/main.c
@@ -1,28 +1,35 @@
 #include <stdbool.h>
 #include <stm8s.h>
-//#include <stdio.h>
+#include <stdio.h>
 #include "main.h"
 #include "milis.h"
 //#include "delay.h"
-//#include "uart1.h"
+#include "uart1.h"
+#define SCALE 10000
+
+
+
+#define RAD_TO_DEG (180.0 / 3.14)
 
     int32_t latitude = 495539350;
     int32_t longitude = 171031850;
     int32_t lockedLatitude = 495539350;
     int32_t lockedLongitude = 171031850;
+    uint16_t lastPWM = 0;
 
-void init(void)
-{
+void init(void){
     init_milis();
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
     GPIO_Init(GPIOD, GPIO_PIN_4, GPIO_MODE_OUT_PP_LOW_SLOW);
 
-    TIM2_TimeBaseInit(TIM2_PRESCALER_256, 1250);
-    TIM2_OC1Init(TIM2_OCMODE_PWM1,TIM2_OUTPUTSTATE_ENABLE, 1 , TIM2_OCPOLARITY_LOW);
+    TIM2_TimeBaseInit(TIM2_PRESCALER_32, 10000);
+    TIM2_OC1Init(TIM2_OCMODE_PWM1,TIM2_OUTPUTSTATE_ENABLE, 1000 , TIM2_OCPOLARITY_HIGH);
     TIM2_OC1PreloadConfig(ENABLE);
     TIM2_Cmd(ENABLE);
-}
 
+    init_uart1();
+}
+/*
 int exp(x,y){
     for (int8_t i=0 ;i <=y ; i++){
         x=x*x;
@@ -38,28 +45,63 @@ int getDiff(now,locked){
 int getAngle(){
     int16_t a = getDiff(latitude,lockedLatitude);
     int16_t b = getDiff(longitude,lockedLongitude);
-    int16_t x = a*100/b
+    int16_t x = a*100/b;
+}
+*/
+
+
+uint16_t toPWM(int32_t degrees){
+    if(degrees>6000){
+        return lastPWM;
+    }else{
+        return degrees*833/10000+500;
+    }
+}
+
+int32_t arctan_fixed(int16_t x, uint8_t N) {
+    int32_t arctan_x = 0;
+    int32_t x_pow = x;  
+
+    for (uint8_t n = 0; n < N; n++) {
+        int16_t term = x_pow / (2 * n + 1);
+        if (n % 2 == 0) {
+            arctan_x += term;
+        } else {
+            arctan_x -= term;
+        }
+        x_pow = (x_pow * x / SCALE) * x / SCALE;
+    }
+
+    return arctan_x*1800/3141;
 }
 
 int main(void)
 {
 
     uint32_t time = 0;
-    uint16_t value = 100;
+    uint16_t value = 500;
+    uint16_t iteration = 0;
+    uint8_t n = 10;
+    uint16_t duty;
 
     init();
 
     while (1) {
-
-
-
-            if (milis() - time > 100 ) {
-                TIM2_SetCompare1(value++);
-
-                time = milis();
-
-                }
-        if (value>1000) {value=10;}
+        iteration = milis();
+        uint16_t x = arctan_fixed(8000,n);
+        //if (milis() - time > 10 ) {
+        //    value=value+10;
+        duty = toPWM(x);
+        TIM2_SetCompare1(duty);
+        //    time = milis();
+        //    }
+        //if (value>1000) {value=500;}
+        iteration = milis()-iteration;
+        if (milis() - time > 1000 ) {
+            time = milis();
+            printf("%d,%d",x,duty);
+            printf("délka smyčky%d,\n",iteration);
+        }
     }
 }
 
